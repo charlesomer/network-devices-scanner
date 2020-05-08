1dbb0f8df84064764908ee94763248da6b6f6152
Charles <charles@zyndex.co.uk>
2020-05-08 18:52:01 +0100

    Refactor.

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Originally developed to switch away mode on/off for Wiser Smart Heating software
 Install the requirements, create the required files and run `script.py`.
 
 Was unable to get nmap to work correctly via docker, see `docker-compose.yaml` for an example docker compose config I have tried. If anyone manages to get this to work please let me know.
+
+Scans every 5 minutes and if no devices have been seen after 15 minutes, send request. This can be changed in config.json as well as other details.
+
+Could do with some cleaning up.
--- script.py
+++ script.py
@@ -2,89 +2,101 @@ import sched, time
 import json
 import nmap3
 import requests
+import copy
+from datetime import datetime, timedelta
 from getmac import get_mac_address
 
 s = sched.scheduler(time.time, time.sleep)
-runEveryXSeconds = 60 * 10
 
 def checkForDevices(sc):
 
-    nmap = nmap3.Nmap()
-    data = None
-    updateJsonFile = 0
-    isSomeoneHome = 0
-    rescanNetwork = 0
-
-    print("START")
+    print("START at " + datetime.now().strftime("%Y-%m-%d %H:%M"))
     print("Checking for devices.")
 
-    with open('./network-devices-scanner/devices.json') as json_file:
-        data = json.load(json_file)
-        for deviceMac in data:
-            print("Scanning for " + data[deviceMac]['name'] + " (" + deviceMac + ") using IP address: " + data[deviceMac]['ip'])
-            tempScan = nmap.scan_top_ports(data[deviceMac]['ip'])
-            print("Scanned.")
-            if data[deviceMac]['ip'] in tempScan:
-                print("Now get mac address of IP address " + data[deviceMac]['ip'])
-                macAddressOfIPOnTheNetwork = get_mac_address(ip=data[deviceMac]['ip']).upper()
-                if macAddressOfIPOnTheNetwork != None:
-                    print("Done. Mac address for " + data[deviceMac]['ip'] + " is " + macAddressOfIPOnTheNetwork)
-                    if macAddressOfIPOnTheNetwork == deviceMac:
-                        # This device is on the network and correct IP is stored in json.
-                        print(data[deviceMac]['name'] + " is on the network and the IP address is " + data[deviceMac]['ip'])
-                        isSomeoneHome = 1
-                    elif macAddressOfIPOnTheNetwork != deviceMac:
-                        print("Stored IP address for " + data[deviceMac]['name'] + " is incorrect and will be updated if available.")
-                        rescanNetwork = 1
-                else:
-                    print("Done. No device exists at IP address " + data[deviceMac]['ip'] + ". JSON will be updated.")
-                    rescanNetwork = 1
-            else:
-                print("IP address is not on the network, rescan to check if it's changed and to determine if device is on network still.")
-                rescanNetwork = 1
-            print()
+    # Open files.
+    with open('./config-files/devices.json') as json_file:
+        devicesJSON = json.load(json_file)
+    with open('./config-files/config.json') as json_file:
+        configJSON = json.load(json_file)
 
-    if rescanNetwork == 1:
+    # Set vars.
+    nmap = nmap3.Nmap()
+    devicesJSONOriginal = copy.deepcopy(devicesJSON)
+    sendAwayRequest = True # Assume no one is home until shown otherwise.
+    rescanNetwork = False
+    minutesBetweenScans = int(configJSON['minutes-between-scans'])
+    minutesToWaitForDeviceToReturn = int(configJSON['minutes-to-wait-for-device-to-return'])
+    usingCronInstead = bool(configJSON['using-cron-instead'])
+
+    # Loop through all devices in devices.json.
+    for deviceMac in devicesJSON:
+        if (datetime.now() - timedelta(minutes=minutesToWaitForDeviceToReturn)) < datetime.strptime(devicesJSON[deviceMac]['lastSeen'], "%Y-%m-%d %H:%M"):
+            sendAwayRequest = False
+            print(devicesJSON[deviceMac]['name'] + " has been on the network in the last " + str(minutesToWaitForDeviceToReturn) + " minutes.")
+        else:
+            print(devicesJSON[deviceMac]['name'] + " has not been on the network in the last " + str(minutesToWaitForDeviceToReturn) + " minutes.")
+
+        print("Scanning for " + devicesJSON[deviceMac]['name'] + " (" + deviceMac + ") using IP address: " + devicesJSON[deviceMac]['ip'])
+        tempScan = nmap.scan_top_ports(devicesJSON[deviceMac]['ip'])
+        print("Scanned.")
+        if devicesJSON[deviceMac]['ip'] in tempScan:
+            print("Now get mac address of IP address " + devicesJSON[deviceMac]['ip'])
+            macAddressOfIPOnTheNetwork = get_mac_address(ip=devicesJSON[deviceMac]['ip']).upper()
+            if macAddressOfIPOnTheNetwork != None:
+                print("Done. Mac address for " + devicesJSON[deviceMac]['ip'] + " is " + macAddressOfIPOnTheNetwork)
+                if macAddressOfIPOnTheNetwork == deviceMac:
+                    # This device is on the network and correct IP is stored in json.
+                    print(devicesJSON[deviceMac]['name'] + " is on the network and the IP address is " + devicesJSON[deviceMac]['ip'])
+                    devicesJSON[deviceMac]['lastSeen'] = datetime.now().strftime("%Y-%m-%d %H:%M")
+                    sendAwayRequest = False
+                elif macAddressOfIPOnTheNetwork != deviceMac:
+                    print("Stored IP address for " + devicesJSON[deviceMac]['name'] + " is incorrect and will be updated if available.")
+                    rescanNetwork = True
+            else:
+                print("Done. No device exists at IP address " + devicesJSON[deviceMac]['ip'] + ". JSON will be updated.")
+                rescanNetwork = True
+        else:
+            print("IP address is not on the network, rescan to check if it's changed and to determine if device is on network still.")
+            rescanNetwork = True
+        print()
+
+    if rescanNetwork == True:
         print("Need to update some IP address, scanning networking now...")
-        with open('./network-devices-scanner/config.json') as json_file:
-            dataFile = json.load(json_file)
-            scan = nmap.scan_top_ports(dataFile['network'])
-            print("Finished scanning network.")
-            for scan_ip in scan:
-                for deviceMac in data:
-                    themac = get_mac_address(ip=scan_ip)
-                    if themac != None and themac.upper() == deviceMac:
-                        print(data[deviceMac]['name'] + " is on the network with IP address " + scan_ip)
-                        isSomeoneHome = 1
-                        if scan_ip != data[deviceMac]['ip']:
-                            data[deviceMac]['ip'] = scan_ip
-                            updateJsonFile = 1
+        scan = nmap.scan_top_ports(configJSON['network'])
+        print("Finished scanning network.")
+        for scan_ip in scan:
+            for deviceMac in devicesJSON:
+                themac = get_mac_address(ip=scan_ip)
+                if themac != None and themac.upper() == deviceMac:
+                    print(devicesJSON[deviceMac]['name'] + " is on the network with IP address " + scan_ip)
+                    sendAwayRequest = False
+                    devicesJSON[deviceMac]['ip'] = scan_ip
+                    devicesJSON[deviceMac]['lastSeen'] = datetime.now().strftime("%Y-%m-%d %H:%M")
 
     print()
-    if updateJsonFile == 1:
+    if devicesJSON != devicesJSONOriginal:
         print("Updating JSON file.")
-        with open('./network-devices-scanner/devices.json', 'w') as outfile:
-            json.dump(data, outfile, indent=4)
+        with open('./config-files/devices.json', 'w') as outfile:
+            json.dump(devicesJSON, outfile, indent=4)
             print("Updated JSON file.")
     else:
         print("JSON file does not need updating.")
 
     print()
-    with open('./network-devices-scanner/config.json') as json_file:
-        data = json.load(json_file)
-        if isSomeoneHome == 1:
-            print("One or more requested devices are on the network. Sending request...")
-            requests.get(data['request-when-devices-are-home'])
-            print("Request sent.")
-        else:
-            print("No requested devices are on the network. Sending request...")
-            requests.get(data['request-when-devices-are-not-home'])
-            print("Request sent.")
+    if sendAwayRequest == False:
+        print("One or more requested devices are on the network. Sending request...")
+        requests.get(configJSON['request-when-devices-are-home'])
+        print("Request sent.")
+    else:
+        print("No requested devices are on the network. Sending request...")
+        requests.get(configJSON['request-when-devices-are-not-home'])
+        print("Request sent.")
 
     print("END")
     print()
 
-    s.enter(runEveryXSeconds, 1, checkForDevices, (sc,))
+    if usingCronInstead != True:
+        s.enter(minutesBetweenScans * 60, 1, checkForDevices, (sc,))
 
 # Don't wait on first run.
 s.enter(1, 1, checkForDevices, (s,))
